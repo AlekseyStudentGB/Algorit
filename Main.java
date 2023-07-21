0ddad123342964cfcc95fa37dbd497694f7d4c8f
Mutniy <wrobotw@mail.ru>
2023-07-21 20:54:32 +0300

    clear

--- Main.java
+++ Main.java
@@ -1,17 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Hash t = new Hash<>();
-        System.out.println(t.getSize());
-        System.out.println(t.getLength());
-        t.add(1,1);
-        t.add(5,5);
-        t.add(4,4);
-        t.add(2,2);
 
-        System.out.println(t.getSize());
-        System.out.println(t.getLength());
-        t.print();
 
     }
 }
